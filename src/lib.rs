d22d4ecae4bcb11eb44db699e7c08fae367cd5c6
Taras Matsyk <x@tar7k.com>
2025-12-11 21:58:28 +0100

    rename project

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use pyo3::prelude::*;
 mod process;
 
 #[pymodule]
-fn rsutil(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn rpsutil(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<process::Process>()?;
     Ok(())
 }
